c1f0f02a381a91415aeebaa2dc39bf423ca5c93f
robsberrywan <yrar93@yahoo.com>
2017-11-14 01:06:27 +0800

    sign out

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { RegisterPage } from '../register/register';
+import { LoginPage } from '../login/login';
+
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Component({
   selector: 'page-home',
@@ -9,11 +12,17 @@ import { RegisterPage } from '../register/register';
 })
 export class HomePage {
 	pages = [
-    	{ title: 'Register', component: RegisterPage }
+    	{ title: 'Register', component: RegisterPage },
+      { title: 'Sign Out', component: LoginPage }
     ];
-	constructor(public navCtrl: NavController) {
-  	}
+	constructor(private afAuth: AngularFireAuth, public navCtrl: NavController) {}
 	openPage(page) {
-    	this.navCtrl.push(page.component);
+      if(page.component==LoginPage){
+        this.afAuth.auth.signOut();
+        this.navCtrl.setRoot(page.component);
+      }
+    	else{
+      this.navCtrl.push(page.component);
+      }
   	}
 }
