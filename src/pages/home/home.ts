cd3da20a41a375238470d420b8578ab1fcc59f80
robsberrywan <yrar93@yahoo.com>
2017-11-13 18:54:25 +0800

    menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RegisterPage } from '../register/register';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	pages = [
+    	{ title: 'Register', component: RegisterPage }
+    ];
+	constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  	}
+	openPage(page) {
+    	this.navCtrl.push(page.component);
+  	}
 }
